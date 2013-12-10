5d756e5de3f27534f7143a78882e9624bf40fa83
Ahoerstemeier <ahoerstemeier@gmail.com>
2013-12-10 05:16:58 -0800

    Entity.setDescription and Entity.removeDescription modified labels, not descriptions

--- Wikibase.NET/Entity.cs
+++ Wikibase.NET/Entity.cs
@@ -13,25 +13,37 @@ namespace Wikibase
         /// <summary>
         /// The entity id
         /// </summary>
-        public EntityId id { get; private set; }
+        public EntityId id
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// The last revision id
         /// </summary>
-        public int lastRevisionId { get; set; }
+        public int lastRevisionId
+        {
+            get;
+            set;
+        }
 
         /// <summary>
         /// The api
         /// </summary>
-        public WikibaseApi api { get; private set; }
+        public WikibaseApi api
+        {
+            get;
+            private set;
+        }
 
-        private Dictionary<string, string> labels = new Dictionary<string,string>();
+        private Dictionary<string, string> labels = new Dictionary<string, string>();
 
-        private Dictionary<string, string> descriptions = new Dictionary<string,string>();
+        private Dictionary<string, string> descriptions = new Dictionary<string, string>();
 
-        private Dictionary<string, List<string>> aliases = new Dictionary<string,List<string>>();
+        private Dictionary<string, List<string>> aliases = new Dictionary<string, List<string>>();
 
-        private Dictionary<string, Dictionary<string, Claim>> claims = new Dictionary<string,Dictionary<string,Claim>>();
+        private Dictionary<string, Dictionary<string, Claim>> claims = new Dictionary<string, Dictionary<string, Claim>>();
 
         protected JsonObject changes = new JsonObject();
 
@@ -39,7 +51,9 @@ namespace Wikibase
         /// Constructor
         /// </summary>
         /// <param name="api">The api</param>
-        public Entity(WikibaseApi api) : this(api, new JsonObject()) { }
+        public Entity(WikibaseApi api) : this(api, new JsonObject())
+        {
+        }
 
         internal Entity(WikibaseApi api, JsonObject data)
         {
@@ -49,52 +63,52 @@ namespace Wikibase
 
         protected virtual void fillData(JsonObject data)
         {
-            if (data.get("id") != null)
+            if ( data.get("id") != null )
             {
                 this.id = EntityId.newFromPrefixedId(data.get("id").asString());
             }
-            if (data.get("lastrevid") != null)
+            if ( data.get("lastrevid") != null )
             {
                 this.lastRevisionId = data.get("lastrevid").asInt();
             }
-            if (data.get("labels") != null)
+            if ( data.get("labels") != null )
             {
                 this.labels = new Dictionary<string, string>();
-                foreach (JsonObject.Member member in data.get("labels").asObject())
+                foreach ( JsonObject.Member member in data.get("labels").asObject() )
                 {
                     JsonObject obj = member.value.asObject();
                     this.labels.Add(obj.get("language").asString(), obj.get("value").asString());
                 }
             }
-            if (data.get("descriptions") != null)
+            if ( data.get("descriptions") != null )
             {
                 this.descriptions = new Dictionary<string, string>();
-                foreach (JsonObject.Member member in data.get("descriptions").asObject())
+                foreach ( JsonObject.Member member in data.get("descriptions").asObject() )
                 {
                     JsonObject obj = member.value.asObject();
                     this.descriptions.Add(obj.get("language").asString(), obj.get("value").asString());
                 }
             }
-            if (data.get("aliases") != null)
+            if ( data.get("aliases") != null )
             {
                 this.aliases = new Dictionary<string, List<string>>();
-                foreach (JsonObject.Member member in data.get("aliases").asObject())
+                foreach ( JsonObject.Member member in data.get("aliases").asObject() )
                 {
                     List<string> list = new List<string>();
-                    foreach (JsonValue value in member.value.asArray())
+                    foreach ( JsonValue value in member.value.asArray() )
                     {
                         list.Add(value.asObject().get("value").asString());
                     }
                     this.aliases.Add(member.name, list);
                 }
             }
-            if (data.get("claims") != null)
+            if ( data.get("claims") != null )
             {
                 this.claims = new Dictionary<string, Dictionary<string, Claim>>();
-                foreach (JsonObject.Member member in data.get("claims").asObject())
+                foreach ( JsonObject.Member member in data.get("claims").asObject() )
                 {
                     Dictionary<string, Claim> list = new Dictionary<string, Claim>();
-                    foreach (JsonValue value in member.value.asArray())
+                    foreach ( JsonValue value in member.value.asArray() )
                     {
                         Claim claim = Claim.newFromArray(this, value.asObject());
                         list.Add(claim.internalId, claim);
@@ -106,9 +120,9 @@ namespace Wikibase
 
         internal static Entity newFromArray(WikibaseApi api, JsonObject data)
         {
-            if (data.get("type") != null)
+            if ( data.get("type") != null )
             {
-                switch (data.get("type").asString())
+                switch ( data.get("type").asString() )
                 {
                     case "item":
                         return new Item(api, data);
@@ -146,7 +160,7 @@ namespace Wikibase
         public void setLabel(string lang, string value)
         {
             this.labels[lang] = value;
-            if (this.changes.get("labels") == null)
+            if ( this.changes.get("labels") == null )
             {
                 this.changes.set("labels", new JsonObject());
             }
@@ -165,9 +179,9 @@ namespace Wikibase
         /// <returns>If the label was removed successfully</returns>
         public bool removeLabel(string lang)
         {
-            if (this.labels.Remove(lang))
+            if ( this.labels.Remove(lang) )
             {
-                if (this.changes.get("labels") == null)
+                if ( this.changes.get("labels") == null )
                 {
                     this.changes.set("labels", new JsonObject());
                 }
@@ -208,8 +222,8 @@ namespace Wikibase
         /// <param name="value">The label</param>
         public void setDescription(string lang, string value)
         {
-            this.labels[lang] = value;
-            if (this.changes.get("descriptions") == null)
+            this.descriptions[lang] = value;
+            if ( this.changes.get("descriptions") == null )
             {
                 this.changes.set("descriptions", new JsonObject());
             }
@@ -228,9 +242,9 @@ namespace Wikibase
         /// <returns>If the description was removed successfully</returns>
         public bool removeDescription(string lang)
         {
-            if (this.labels.Remove(lang))
+            if ( this.descriptions.Remove(lang) )
             {
-                if (this.changes.get("descriptions") == null)
+                if ( this.changes.get("descriptions") == null )
                 {
                     this.changes.set("descriptions", new JsonObject());
                 }
@@ -252,7 +266,7 @@ namespace Wikibase
         public Dictionary<string, List<string>> getAliases()
         {
             Dictionary<string, List<string>> copy = new Dictionary<string, List<string>>(aliases);
-            foreach (KeyValuePair<string, List<string>> pair in copy)
+            foreach ( KeyValuePair<string, List<string>> pair in copy )
             {
                 copy[pair.Key] = new List<string>(pair.Value);
             }
@@ -276,20 +290,20 @@ namespace Wikibase
         /// <param name="value">The alias</param>
         public void addAlias(string lang, string value)
         {
-            if (!this.aliases.ContainsKey(lang))
+            if ( !this.aliases.ContainsKey(lang) )
             {
                 this.aliases.Add(lang, new List<string>());
             }
             this.aliases[lang].Add(value);
-            if (this.changes.get("aliases") == null)
+            if ( this.changes.get("aliases") == null )
             {
                 this.changes.set("aliases", new JsonArray());
             }
             // Override if needed an action on the same alias
-            for(int i = 0; i < this.changes.get("aliases").asArray().size(); i++)
+            for ( int i = 0 ; i < this.changes.get("aliases").asArray().size() ; i++ )
             {
                 JsonObject obj = this.changes.get("aliases").asArray().get(i).asObject();
-                if (obj.get("language").asString() == lang && obj.get("value").asString() == value)
+                if ( obj.get("language").asString() == lang && obj.get("value").asString() == value )
                 {
                     this.changes.get("aliases").asArray().removeAt(i);
                     break;
@@ -311,19 +325,19 @@ namespace Wikibase
         /// <returns>If the alias was removed successfully</returns>
         public bool removeAlias(string lang, string value)
         {
-            if (this.aliases.ContainsKey(lang))
+            if ( this.aliases.ContainsKey(lang) )
             {
-                if (this.aliases[lang].Remove(value))
+                if ( this.aliases[lang].Remove(value) )
                 {
-                    if (this.changes.get("aliases") == null)
+                    if ( this.changes.get("aliases") == null )
                     {
                         this.changes.set("aliases", new JsonArray());
                     }
                     // Override if needed an action on the same alias
-                    for (int i = 0; i < this.changes.get("aliases").asArray().size(); i++)
+                    for ( int i = 0 ; i < this.changes.get("aliases").asArray().size() ; i++ )
                     {
                         JsonObject obj = this.changes.get("aliases").asArray().get(i).asObject();
-                        if (obj.get("language").asString() == lang && obj.get("value").asString() == value)
+                        if ( obj.get("language").asString() == lang && obj.get("value").asString() == value )
                         {
                             this.changes.get("aliases").asArray().removeAt(i);
                             break;
@@ -348,7 +362,7 @@ namespace Wikibase
         public Dictionary<string, Dictionary<string, Claim>> getClaims()
         {
             Dictionary<string, Dictionary<string, Claim>> copy = new Dictionary<string, Dictionary<string, Claim>>(claims);
-            foreach (KeyValuePair<string, Dictionary<string, Claim>> pair in copy)
+            foreach ( KeyValuePair<string, Dictionary<string, Claim>> pair in copy )
             {
                 copy[pair.Key] = new Dictionary<string, Claim>(pair.Value);
             }
@@ -372,9 +386,9 @@ namespace Wikibase
         internal void addClaim(Claim claim)
         {
             string property = claim.mainSnak.propertyId.getPrefixedId();
-            if(!this.claims.ContainsKey(property))
+            if ( !this.claims.ContainsKey(property) )
             {
-                this.claims[property] = new Dictionary<string,Claim>();
+                this.claims[property] = new Dictionary<string, Claim>();
             }
             this.claims[property][claim.internalId] = claim;
         }
@@ -387,13 +401,13 @@ namespace Wikibase
         internal bool removeClaim(Claim claim)
         {
             string property = claim.mainSnak.propertyId.getPrefixedId();
-            if (!this.claims.ContainsKey(property))
+            if ( !this.claims.ContainsKey(property) )
             {
                 return false;
             }
-            if (this.claims[property].Remove(claim.internalId))
+            if ( this.claims[property].Remove(claim.internalId) )
             {
-                if (this.claims[property].Count == 0)
+                if ( this.claims[property].Count == 0 )
                 {
                     this.claims.Remove(property);
                 }
@@ -408,10 +422,10 @@ namespace Wikibase
         /// <param name="summary">The edit summary</param>
         public void save(string summary)
         {
-            if (!this.changes.isEmpty())
+            if ( !this.changes.isEmpty() )
             {
                 JsonObject result;
-                if(this.id == null)
+                if ( this.id == null )
                 {
                     result = this.api.createEntity(this.getType(), this.changes, this.lastRevisionId, summary);
                 }
@@ -419,7 +433,7 @@ namespace Wikibase
                 {
                     result = this.api.editEntity(this.id.getPrefixedId(), this.changes, this.lastRevisionId, summary);
                 }
-                if (result.get("entity") != null)
+                if ( result.get("entity") != null )
                 {
                     this.fillData(result.get("entity").asObject());
                 }
@@ -430,7 +444,7 @@ namespace Wikibase
 
         internal void updateLastRevisionIdFromResult(JsonObject result)
         {
-            if (result.get("pageinfo") != null && result.get("pageinfo").asObject().get("lastrevid") != null)
+            if ( result.get("pageinfo") != null && result.get("pageinfo").asObject().get("lastrevid") != null )
             {
                 this.lastRevisionId = result.get("pageinfo").asObject().get("lastrevid").asInt();
             }
